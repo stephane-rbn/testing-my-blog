f78b8deaa8f806e6ac4900a17395e6bbd689e67e
Stéphane <rabena.stephane@gmail.com>
2020-06-25 01:07:07 +0200

    Add Article model attributes validations + Article tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :is_draft, inclusion: { in: [true, false] }
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(title: 'My first blog', content: 'Lorem ipsum', is_draft: false) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a content' do
+    subject.content = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a is_draft' do
+    subject.is_draft = nil
+    expect(subject).to_not be_valid
+  end
 end
