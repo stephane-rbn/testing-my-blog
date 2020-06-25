22c1ba2d5256e8e8d1c0429b6d363f6398ed5521
Stéphane <rabena.stephane@gmail.com>
2020-06-25 02:45:44 +0200

    Add User model tests to test name presence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(
+      email: 'john@doe.com',
+      first_name: 'John',
+      last_name: 'Doe',
+      password: '$tr0Ng;p@ssWord{42}',
+      password_confirmation: '$tr0Ng;p@ssWord{42}'
+    )
+  end
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a first_name' do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a last_name' do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an identical password_confirmation' do
+      subject.password_confirmation = '$tr0Ng;p@ssWord{43}'
+      expect(subject).to_not be_valid
+    end
+  end
 end
