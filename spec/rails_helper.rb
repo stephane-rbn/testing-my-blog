a79b018f3fa3ba1e28f01f35a6b974acbe045937
Stéphane <rabena.stephane@gmail.com>
2020-06-28 00:15:49 +0200

    Modify Capybara default driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
 
-# Capybara.default_driver = :selenium_chrome
+Capybara.default_driver = :selenium_chrome_headless
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
