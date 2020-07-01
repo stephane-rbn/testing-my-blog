3a58805e1567f36b6aa3fb6ad3e09412153410d0
Stéphane <rabena.stephane@gmail.com>
2020-07-01 14:10:53 +0200

    Add failure spec

--- spec/system/articles/user_creates_an_article_spec.rb
+++ spec/system/articles/user_creates_an_article_spec.rb
@@ -4,74 +4,86 @@ RSpec.describe 'Create Article', type: :system do
   let(:user) { FactoryBot.create(:user) }
   let(:article) { FactoryBot.create(:article, user: user) }
 
-  before(:each) { sign_in user }
+  context 'on success' do
+    before(:each) { sign_in user }
 
-  context 'with success' do
-    scenario 'by visiting the new page' do
-      visit new_article_path
-      expect(page).to have_content('Sign out')
-      expect(page).to have_current_path(new_article_path)
-      expect(page).to have_field('Title')
-      expect(page).to have_field('Content')
-      sleep(1)
-      within('form') do
-        fill_in 'article[title]', with: 'How to create an article with a robot'
-        fill_in 'article[content]', with: 'This article is written by a robot'
+    describe 'as an authenticated user' do
+      scenario 'by visiting the new page' do
+        visit new_article_path
+        expect(page).to have_content('Sign out')
+        expect(page).to have_current_path(new_article_path)
+        expect(page).to have_field('Title')
+        expect(page).to have_field('Content')
+        sleep(1)
+        within('form') do
+          fill_in 'article[title]', with: 'How to create an article with a robot'
+          fill_in 'article[content]', with: 'This article is written by a robot'
+        end
+        sleep(1)
+        click_button 'Create Article'
+        expect(page).to have_content('New article successfully added!')
+        expect(page).to have_content('How to create an article with a robot')
+        expect(page).to have_content('This article is written by a robot')
+        expect(page).to have_link('Home', href: '/articles')
+        sleep(1)
       end
-      sleep(1)
-      click_button 'Create Article'
-      expect(page).to have_content('New article successfully added!')
-      expect(page).to have_content('How to create an article with a robot')
-      expect(page).to have_content('This article is written by a robot')
-      expect(page).to have_link('Home', href: '/articles')
-      sleep(1)
+
+      scenario 'by visiting home page' do
+        visit articles_path
+        expect(page).to have_content('Articles')
+        sleep(1)
+        click_link('New')
+        expect(page).to have_current_path(new_article_path)
+        expect(page).to have_field('Title')
+        expect(page).to have_field('Content')
+        sleep(1)
+        within('form') do
+          fill_in 'article[title]', with: 'How to create an article with a robot'
+          fill_in 'article[content]', with: 'This article is written by a robot'
+        end
+        sleep(1)
+        click_button 'Create Article'
+        expect(page).to have_content('New article successfully added!')
+        expect(page).to have_content('How to create an article with a robot')
+        expect(page).to have_content('This article is written by a robot')
+        expect(page).to have_link('Home', href: '/articles')
+        sleep(1)
+      end
+
+      scenario 'by visiting show page' do
+        visit article_path(article)
+        expect(page).to have_content(article.title)
+        sleep(1)
+        visit articles_path
+        expect(page).to have_content('Articles')
+        sleep(1)
+        click_link('New')
+        expect(page).to have_current_path(new_article_path)
+        expect(page).to have_field('Title')
+        expect(page).to have_field('Content')
+        sleep(1)
+        within('form') do
+          fill_in 'article[title]', with: 'How to create an article with a robot'
+          fill_in 'article[content]', with: 'This article is written by a robot'
+        end
+        sleep(1)
+        click_button 'Create Article'
+        expect(page).to have_content('New article successfully added!')
+        expect(page).to have_content('How to create an article with a robot')
+        expect(page).to have_content('This article is written by a robot')
+        expect(page).to have_link('Home', href: '/articles')
+        sleep(1)
+      end
     end
+  end
 
-    scenario 'by visiting home page' do
-      visit articles_path
-      expect(page).to have_content('Articles')
-      sleep(1)
-      click_link('New')
-      expect(page).to have_current_path(new_article_path)
-      expect(page).to have_field('Title')
-      expect(page).to have_field('Content')
-      sleep(1)
-      within('form') do
-        fill_in 'article[title]', with: 'How to create an article with a robot'
-        fill_in 'article[content]', with: 'This article is written by a robot'
+  context 'on failure' do
+    describe 'as an anonymous user' do
+      scenario 'by visiting the new page' do
+        visit new_article_path
+        expect(page).to have_content('You need to sign in or sign up before continuing.')
+        sleep(1)
       end
-      sleep(1)
-      click_button 'Create Article'
-      expect(page).to have_content('New article successfully added!')
-      expect(page).to have_content('How to create an article with a robot')
-      expect(page).to have_content('This article is written by a robot')
-      expect(page).to have_link('Home', href: '/articles')
-      sleep(1)
-    end
-
-    scenario 'by visiting show page' do
-      visit article_path(article)
-      expect(page).to have_content(article.title)
-      sleep(1)
-      visit articles_path
-      expect(page).to have_content('Articles')
-      sleep(1)
-      click_link('New')
-      expect(page).to have_current_path(new_article_path)
-      expect(page).to have_field('Title')
-      expect(page).to have_field('Content')
-      sleep(1)
-      within('form') do
-        fill_in 'article[title]', with: 'How to create an article with a robot'
-        fill_in 'article[content]', with: 'This article is written by a robot'
-      end
-      sleep(1)
-      click_button 'Create Article'
-      expect(page).to have_content('New article successfully added!')
-      expect(page).to have_content('How to create an article with a robot')
-      expect(page).to have_content('This article is written by a robot')
-      expect(page).to have_link('Home', href: '/articles')
-      sleep(1)
     end
   end
 end
