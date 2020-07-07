fcf63f0b67c3a5c56d3f932e7a2b2f530fa15d60
Stanislas Lange <stanislas.lange@pm.me>
2020-07-07 14:54:54 +0200

    wip

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -88,5 +88,21 @@ RSpec.configure do |config|
   #   end
   # end
 
-  Capybara.current_driver = :selenium_chrome_headless
+  # Capybara.current_driver = :selenium_chrome_headless
+  # driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
+
+  Capybara.register_driver :selenium_chrome_headless_docker_friendly do |app|
+    Capybara::Selenium::Driver.load_selenium
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new
+    browser_options.args << '--headless'
+    browser_options.args << '--disable-gpu'
+    # Sandbox cannot be used inside unprivileged Docker container
+    browser_options.args << '--no-sandbox'
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+  end
+
+  Capybara.javascript_driver = :selenium_chrome_headless_docker_friendly
+  Capybara.current_driver = :selenium_chrome_headless_docker_friendly
+  Capybara.default_driver = :selenium_chrome_headless_docker_friendly
+
 end
